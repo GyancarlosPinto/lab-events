df9de050ea5b6404415efe2dfc440ad16f1f604d
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-11-01 22:41:38 -0400

    Finished Pixel-Painter Functions

---
+++ pixel-painter/main.js
@@ -0,0 +1,25 @@
+function createCells() {
+    for (let i = 0; i < 225; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.addEventListener("click", () => {
+            const newColor = document.querySelector("#current-color");
+            cell.style.background = newColor.style.background;
+        });
+        const grid = document.querySelector("#canvas")
+        grid.appendChild(cell);
+    }
+}
+
+function changingCurrentColor(params) {
+    const colors = document.querySelectorAll("#palette .color");
+    for (const color of colors) {
+        color.addEventListener("click", () => {
+            const newColor = document.querySelector("#current-color");
+            newColor.style.background = color.style.background;
+        });
+    }
+}
+
+createCells();
+changingCurrentColor();
